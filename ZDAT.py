8ff871b726380bd8fac30013d384d2510c4c13db
Chips <105132829+Elysia-simp@users.noreply.github.com>
2024-12-18 01:06:58 +0900

    Implement brute force decrypt

--- ZDAT.py
+++ ZDAT.py
@@ -1,4 +1,13 @@
-import struct
+import struct, os
+
+if not os.path.exists("output"):
+    os.makedirs("output", exist_ok=True) # i don't know why i didn't do this...
+
+def xor_decrypt(data, key):
+
+    get_key = bytes([data[0] ^ key[0]])
+
+    return bytes(x ^ get_key[i % len(get_key)] for i, x in enumerate(data))
 
 with open('zdat here', 'rb') as file:
     ms = file
@@ -28,7 +37,8 @@ with open('zdat here', 'rb') as file:
         ms.seek(Data_offset)
 
         file_bytes = ms.read(FileSize)
-
+        if file_bytes[:7] != b"UnityFS": #they have several but only the one is used
+            file_bytes = xor_decrypt(file_bytes, b"U")
         with open(f"output/{name_string}", 'wb') as f:
             f.write(file_bytes)
         print(f"Written {name_string}")
